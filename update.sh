b593a59682a7c906b818aa86136fc830ebfe03aa
oddlyspaced <oddlyspaced1@gmail.com>
2019-02-11 01:16:04 +0530

    Added rofi config support

--- update.sh
+++ update.sh
@@ -5,3 +5,4 @@ cp compton-config ~/.config/compton.conf
 cp polybar-config ~/.config/polybar/config
 cp polybar-launch.sh ~/.config/polybar/launch.sh
 cp dunst-config ~/.config/dunst/dunstrc
+cp rofi-config ~/.config/rofi/config
