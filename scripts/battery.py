c8b8ef913bcaf17ed65691e541c0bd8078538a48
oddlyspaced <oddlyspaced1@gmail.com>
2019-02-10 23:46:05 +0530

    Make battery script better functioning Always make it notify when battery <=15% 	modified:   scripts/battery.py

--- scripts/battery.py
+++ scripts/battery.py
@@ -5,6 +5,6 @@ battery = psutil.sensors_battery()
 while True :
     percent = int(battery.percent)
     is_charging = battery.power_plugged
-    if is_charging is False and percent is 15 :
-        os.system ("dunstify \"Battery at 15% please connect charger!\"")
+    if is_charging is False and percent <= 15 :
+        os.system ("dunstify \"Battery at " + str(percent) + "% please connect charger!\"")
     time.sleep(10)
