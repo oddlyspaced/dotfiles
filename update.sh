4a46db6e56ba3fb0ef198c99e2f8142c6513fb87
oddlyspaced <oddlyspaced1@gmail.com>
2019-02-10 21:16:30 +0530

    Rename launch.sh and reflect changes in updater.sh 	renamed:    launch.sh -> polybar-launch.sh 	modified:   update.sh

--- update.sh
+++ update.sh
@@ -3,3 +3,4 @@ cp i3-config ~/.config/i3/config
 cp termite-config ~/.config/termite/config
 cp compton-config ~/.config/compton.conf
 cp polybar-config ~/.config/polybar/config
+cp polybar-launch.sh ~/.config/polybar/launch.sh
