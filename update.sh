573a62cb4e73cdfaac53ec1730453107e3167300
oddlyspaced <oddlyspaced1@gmail.com>
2019-02-10 22:43:37 +0530

    Reflect dunst changes in update.sh 	modified:   update.sh

--- update.sh
+++ update.sh
@@ -4,3 +4,4 @@ cp termite-config ~/.config/termite/config
 cp compton-config ~/.config/compton.conf
 cp polybar-config ~/.config/polybar/config
 cp polybar-launch.sh ~/.config/polybar/launch.sh
+cp dunst-config ~/.config/dunst/dunstrc
