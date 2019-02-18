ba992223b52604958b95c9d1c88da04f100d352c
oddlyspaced <oddlyspaced1@gmail.com>
2019-02-18 18:12:01 +0530

    Create directories

--- update.sh
+++ update.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+mkdir ~/.config/i3
 cp i3-config ~/.config/i3/config
+mkdir ~/.config/termite
 cp termite-config ~/.config/termite/config
 cp compton-config ~/.config/compton.conf
+mkdir ~/.config/polybar
 cp polybar-config ~/.config/polybar/config
 cp polybar-launch.sh ~/.config/polybar/launch.sh
+mkdir ~/.config/dunst
 cp dunst-config ~/.config/dunst/dunstrc
+mkdir ~/.config/rofi
 cp rofi-config ~/.config/rofi/config
