ec5ca41d6f6880559e9020cfe4bf980077b6ad44
oddlyspaced <oddlyspaced1@gmail.com>
2019-03-27 23:03:52 +0530

    Add install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Installing packages..."
+sudo pacman -S conky termite nitrogen papirus-icon-theme
+yay -S polybar compton-tryone-git ttf-comfortaa nerd-fonts-ubuntu-mono ttf-google-sans ant-dracula-gtk-theme i3lock-fancy-git
+nitrogen --set-zoom-fill wallpaper.jpg
+
+echo "Setting configs..."
+cp -r .config/compton.conf ~/.config/
+cp -r .config/conky/ ~/.config/
+cp -r .config/i3/ ~/.config/
+cp -r .config/polybar/ ~/.config/
+cp -r .config/termite/ ~/.config/
+
+echo "All Done!\nPlease go ahead and use lxappearance to set gtk theme to Ant Dracula. Also replace wlo1 with your wifi interface in polybar config"
